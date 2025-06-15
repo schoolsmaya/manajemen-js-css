c2e4b3e668675538d75e1a6310e0de75bfc68c2c
School Smaya <admin@schoolyapip-smaya.sch.id>
2025-06-15 23:09:02 +0800

    Create domElements.js

---
+++ resources/post/js/domElements.js
@@ -0,0 +1,21 @@
+// domElements.js
+const identitasForm = document.getElementById('identitas-form');
+const soalContainer = document.getElementById('soal-container');
+const hasilContainer = document.getElementById('hasil-container');
+
+const namaInput = document.getElementById('nama');
+const jabatanSelect = document.getElementById('jabatan');
+const kunciAksesInput = document.getElementById('kunciAkses');
+const accessErrorMsg = document.getElementById('accessError');
+
+const nomorSoalEl = document.getElementById('nomor-soal');
+const teksSoalEl = document.getElementById('teks-soal');
+const opsiJawabanEl = document.getElementById('opsi-jawaban');
+
+const prevbtns = document.getElementById('prevbtns');
+const nextbtns = document.getElementById('nextbtns');
+const submitbtns = document.getElementById('submitbtns');
+const masukSoalbtns = document.getElementById('masukSoal');
+const ulangKuisbtns = document.getElementById('ulangKuis');
+
+// export { identitasForm, soalContainer, hasilContainer, /* ... dan seterusnya */ };
