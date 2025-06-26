4d4723854c4f1dc26c4a0353ba9352e48231b6bc
School Smaya <admin@schoolyapip-smaya.sch.id>
2025-06-27 00:42:30 +0800

    Add files via upload

--- resources/page/generator/index.js
+++ resources/page/generator/index.js
@@ -1,3 +1,5 @@
+
+
 console.log("hello")
 const userName = document.getElementById("name");
 const submitBtn = document.getElementById("submitBtn");
@@ -22,7 +24,7 @@ const generatePDF = async (name) => {
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     pdfDoc.registerFontkit(fontkit);
 
-
+    
   //get font
   const fontBytes = await fetch("Sanchez-Regular.ttf").then((res) =>
   res.arrayBuffer()
@@ -32,7 +34,7 @@ const generatePDF = async (name) => {
    // Get the first page of the document
    const pages = pdfDoc.getPages();
    const firstPage = pages[0];
-
+ 
    // Draw a string of text diagonally across the first page
    firstPage.drawText(name, {
      x: 300,
@@ -41,8 +43,9 @@ const generatePDF = async (name) => {
      font: SanChezFont ,
      color: rgb(0.2, 0.84, 0.67),
    });
-
+ 
   // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
   saveAs(pdfDataUri,"newcertificate.pdf")
 };
+
